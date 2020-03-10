44ea25c38e6334d99d7f778e752aeb81c36ca0c5
Cy Hayakawa <cy.hayakawa@gmail.com>
2020-03-10 18:57:57 -0300

    Inserção de usuário com POST

--- src/main/java/com/testetey/workshopmongo/services/UserService.java
+++ src/main/java/com/testetey/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.testetey.workshopmongo.domain.User;
+import com.testetey.workshopmongo.dto.UserDto;
 import com.testetey.workshopmongo.repository.UserRepository;
 import com.testetey.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,13 @@ public class UserService {
 		
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));		
+	}
+	
+	public User insert (User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDto(UserDto objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
